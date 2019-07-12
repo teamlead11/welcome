76d4b9042f2a1245882dcd5832682c5ec4648795
ktrinath <gnanasekar7195@gmail.com>
2019-07-12 17:59:49 +0530

    new packages added

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,27 @@
+package org.company;
+
+public class CompanyInfo {
+	public void companyName() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens Technology");
+
+	}
+	public void companyId() {
+		// TODO Auto-generated method stub
+		System.out.println("GT-136");
+
+	}
+	public void companyAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("Near ICICI Bank, OMR Road, Chennai");
+
+	}
+	public static void main(String[] args) {
+		CompanyInfo c = new CompanyInfo();
+		c.companyName();
+		c.companyAddress();
+		c.companyId();
+			}
+	
+
+}
